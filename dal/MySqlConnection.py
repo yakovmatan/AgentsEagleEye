896bc946fef23397eb0602c919837fd3284fd288
yakov matan <yakovmatan@gmail.com>
2025-06-26 11:58:14 +0300

    create connection dal

---
+++ dal/MySqlConnection.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+import mysql.connector
+
+class MySQLConnection:
+    def __init__(self, host, user, password, database):
+        self.config = {
+            "host": host,
+            "user": user,
+            "password": password,
+            "database": database
+        }
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        try:
+            self.conn = mysql.connector.connect(**self.config)
+            self.cursor = self.conn.cursor(dictionary=True)
+            return self
+        except mysql.connector.Error as err:
+            print(f"Connection Error: {err}")
+            raise
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
+        if exc_type:
+            print(f"Exception occurred: {exc_type} - {exc_val}")
