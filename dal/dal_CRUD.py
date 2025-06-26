b5978e3a9b333c325bf8cc74be0fb02ebb5cf52c
yakov matan <yakovmatan@gmail.com>
2025-06-26 13:40:11 +0300

    add format with to class dal_CRUD

--- dal/dal_CRUD.py
+++ dal/dal_CRUD.py
@@ -1,46 +1,57 @@
+from webbrowser import Error
+
 import mysql
 from dal.MySqlConnection import MySQLConnection
 
 
 class DalCrud:
 
-    def __init__(self,connection: MySQLConnection):
-        self.connection = connection
+    def __init__(self,host, user, password, database):
+        self.db_config = {
+            "hose": host,
+            "user": user,
+            "password": password,
+            "database": database
+        }
 
     def select(self,query,params=None):
         try:
-            self.connection.cursor.execute(query,params)
-            return self.connection.cursor.fetchall()
-        except mysql.connector.Error as err:
-            print(f"SELECT Error: {err}")
-            return []
+            with MySQLConnection(**self.db_config) as connection:
+                connection.cursor.execute(query,params)
+                return connection.cursor.fetchall()
+        except Error as e:
+            print(f"SELECT Error: {e}")
+            return None
 
     def insert(self,query,params=None):
         try:
-            self.connection.cursor.execute(query,params)
-            self.connection.conn.commit()
-            print(f"Insert successful. ")
-            return self.connection.cursor.lastrowid
-        except mysql.connector.Error as err:
+            with MySQLConnection(**self.db_config) as connection:
+                connection.cursor.execute(query,params)
+                connection.conn.commit()
+                print(f"Insert successful. ")
+                return connection.cursor.lastrowid
+        except Error as err:
             print(f"Insert Error: {err}")
             return None
 
     def update(self,query, params=None):
         try:
-            self.connection.cursor.execute(query, params)
-            self.connection.conn.commit()
-            print("Update successful")
-            return self.connection.cursor.rowcount
-        except mysql.connector.Error as err:
+            with MySQLConnection(**self.db_config) as connection:
+                connection.cursor.execute(query, params)
+                connection.conn.commit()
+                print("Update successful")
+                return connection.cursor.rowcount
+        except Error as err:
             print(f"Update Error: {err}")
             return 0
 
     def delete(self, query, params=None):
         try:
-            self.connection.cursor.execute(query, params)
-            self.connection.conn.commit()
-            print("Delete successful")
-            return self.connection.cursor.rowcount
-        except mysql.connector.Error as err:
+            with MySQLConnection(**self.db_config) as connection:
+                connection.cursor.execute(query, params)
+                connection.conn.commit()
+                print("Delete successful")
+                return connection.cursor.rowcount
+        except Error as err:
             print(f"Delete Error: {err}")
             return 0
